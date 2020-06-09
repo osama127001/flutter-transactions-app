359a7c3e74f25c9d671baf16e6aebb3a1a18a694
Osama Khan <contactosamakhan@gmail.com>
2020-06-09 14:25:55 +0500

    added image from assets

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -2,72 +2,81 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:transactions/models/transaction.dart';
 
-
 class TransactionList extends StatelessWidget {
-    
   // initializing transaction lists to get from user transsctions
   final List<Transaction> transactions;
 
   // constructor that recieves transactions lists
   TransactionList(this.transactions);
 
-
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 400,
-      child: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Column(
-              children: transactions.map((tx) {
-                return Card(
-                  elevation: 10,
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 20,
+      child: transactions.isEmpty
+          ? Column(
+              children: <Widget>[
+                Text("No Transactions"),
+                Container(
+                  height: 200,
+                  child: Image.asset('assets/images/none.png'),
+                ),
+              ],
+            )
+          : SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Column(
+                    children: transactions.map((tx) {
+                      return Card(
+                        elevation: 10,
+                        child: Row(
+                          children: <Widget>[
+                            Container(
+                              margin: EdgeInsets.symmetric(
+                                vertical: 10,
+                                horizontal: 20,
+                              ),
+                              decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: Colors.blue, width: 3),
+                              ),
+                              padding: EdgeInsets.symmetric(
+                                vertical: 10,
+                                horizontal: 10,
+                              ),
+                              child: Text(
+                                '\$${tx.ammount}',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Theme.of(context).primaryColor,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  tx.title,
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  DateFormat.yMMMd().format(tx.date),
+                                  style: TextStyle(color: Colors.green),
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.blue, width: 3),
-                        ),
-                        padding: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 10,
-                        ),
-                        child: Text(
-                          '\$${tx.ammount}',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Theme.of(context).primaryColor,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            tx.title,
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            DateFormat.yMMMd().format(tx.date),
-                            style: TextStyle(color: Colors.green),
-                          ),
-                        ],
-                      ),
-                    ],
+                      );
+                    }).toList(),
                   ),
-                );
-              }).toList(),
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
     );
   }
 }
--- lib/widgets/user_transactions.dart
+++ lib/widgets/user_transactions.dart
@@ -10,9 +10,9 @@ class UserTransactions extends StatefulWidget {
 
 class _UserTransactionsState extends State<UserTransactions> {
   final List<Transaction> _userTransactions = [
-    Transaction(id: 1, title: 'Shoes', ammount: 5000, date: DateTime.now()),
-    Transaction(id: 2, title: 'Suit', ammount: 9000, date: DateTime.now()),
-    Transaction(id: 3, title: 'IPhoneX', ammount: 90000, date: DateTime.now()),
+    // Transaction(id: 1, title: 'Shoes', ammount: 5000, date: DateTime.now()),
+    // Transaction(id: 2, title: 'Suit', ammount: 9000, date: DateTime.now()),
+    // Transaction(id: 3, title: 'IPhoneX', ammount: 90000, date: DateTime.now()),
   ];
 
   void _addNewTransaction(String txTitle, double txAmmount) {
