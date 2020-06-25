dc24c912e91ea7927c3839507c690a32e0054b2d
Osama Khan <contactosamakhan@gmail.com>
2020-06-25 12:33:58 +0500

    adding alert box to delete transaction

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -34,9 +34,37 @@ class TransactionList extends StatelessWidget {
                       return ListTile(
                         title: Text('${tx.title}'),
                         subtitle: Text(DateFormat.yMMMd().format(tx.date)),
-                        trailing: Text('\$${tx.ammount}'), 
-                        onTap: (){},
-                      );
+                        trailing: Text('\$${tx.ammount}'),
+                        onTap: () => showDialog<void>(
+                      context: context,
+                      barrierDismissible: false, // user must tap button!
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('Edit Transaction'),
+                          content: SingleChildScrollView(
+                            child: ListBody(
+                              children: <Widget>[
+                                Text('Would you like delete this transaction?'),
+                              ],
+                            ),
+                          ),
+                          actions: <Widget>[
+                            FlatButton(
+                              child: Text('Delete'),
+                              onPressed: () {
+                                deleteTx(tx.id);
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                            FlatButton(
+                              child: Text('Donot Delete'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );}
+                      ));
                     }).toList(),
                   ),
                 ],
